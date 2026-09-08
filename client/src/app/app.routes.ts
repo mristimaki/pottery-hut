2b3a0dc5a8935c55e9980ecba28474b5d4eeab12
m.ristimaki <rosanna.ristimaki@gmail.com>
2026-09-08 16:11:18 +0200

    Add basket route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { SearchResults } from './pages/search-results/search-results';
 import { ProductDetail } from './pages/product-detail/product-detail';
+import { Basket } from './pages/basket/basket';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'search', component: SearchResults },
-    { path: 'products/:slug', component: ProductDetail }
+    { path: 'products/:slug', component: ProductDetail },
+    { path: 'basket', component: Basket }
 ];
