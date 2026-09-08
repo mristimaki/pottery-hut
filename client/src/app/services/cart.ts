012ec9ebcea60f9eb53d0e600bed6f768f98a0a5
m.ristimaki <rosanna.ristimaki@gmail.com>
2026-09-08 16:09:47 +0200

    Add Cart service with quantity and total price management

--- client/src/app/services/cart.ts
+++ client/src/app/services/cart.ts
@@ -10,9 +10,15 @@ export interface CartItem {
 export class Cart {
     private items = signal<CartItem[]>([]);
 
+    cartItems = computed(() => this.items());
+
     totalItemCount = computed(() =>
     this.items().reduce((sum, item) => sum + item.quantity, 0)
-);
+    );
+
+    totalPrice = computed(() =>
+        this.items().reduce((sum, item) => sum + item.product.price * item.quantity, 0)
+    );
 
     addToCart(product: Product) {
         const existing = this.items().find(item => item.product.id === product.id);
@@ -28,5 +34,19 @@ export class Cart {
         } else {
             this.items.update(current => [...current, { product, quantity: 1 }])
         }
+    }
+
+    removeFromCart(productId: number) {
+        this.items.update(current => current.filter(item => item.product.id !== productId));
+    }
+
+    updateQuantity(productId: number, quantity: number) {
+        this.items.update(current => 
+            current.map(item =>
+                item.product.id === productId
+                ? { ...item, quantity: quantity }
+                : item
+            )
+        );
     }
 }
