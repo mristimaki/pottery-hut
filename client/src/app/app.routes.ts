0111221d8586d2f903002bc9fe9c483f6cab76cb
m.ristimaki <rosanna.ristimaki@gmail.com>
2026-09-09 17:33:10 +0200

    Add admin products route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SearchResults } from './pages/search-results/search-results';
 import { ProductDetail } from './pages/product-detail/product-detail';
 import { Basket } from './pages/basket/basket';
 import { Checkout } from './pages/checkout/checkout';
+import { AdminProductList } from './pages/admin-product-list/admin-product-list';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'search', component: SearchResults },
     { path: 'products/:slug', component: ProductDetail },
     { path: 'basket', component: Basket },
-    { path: 'checkout', component: Checkout }
+    { path: 'checkout', component: Checkout },
+    { path: 'admin/products', component: AdminProductList }
 ];
