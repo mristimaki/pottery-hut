6824ae813bc81057dc99b64f7c6f630e5edaea9f
m.ristimaki <rosanna.ristimaki@gmail.com>
2026-09-09 15:55:30 +0200

    Add checkout route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './pages/home/home';
 import { SearchResults } from './pages/search-results/search-results';
 import { ProductDetail } from './pages/product-detail/product-detail';
 import { Basket } from './pages/basket/basket';
+import { Checkout } from './pages/checkout/checkout';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'search', component: SearchResults },
     { path: 'products/:slug', component: ProductDetail },
-    { path: 'basket', component: Basket }
+    { path: 'basket', component: Basket },
+    { path: 'checkout', component: Checkout }
 ];
